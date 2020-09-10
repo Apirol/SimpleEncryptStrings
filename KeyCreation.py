51e6cf468b5fe629977a97066ec2dd50a77103f1
Redut Anatoly <56587883+Apirol@users.noreply.github.com>
2020-09-10 16:39:04 +0700

    Added gitignore

--- KeyCreation.py
+++ KeyCreation.py
@@ -1,10 +1,11 @@
-from random import randint, random, seed
+from random import randint
 
-key = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя') ## default key
+key = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')  # default key
 alphabet = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
 lenghtOfKey = len(key) - 1
 
-def keyCreation ():
+
+def keyCreation():
     for i in range(lenghtOfKey - 1):
         j = randint(0, lenghtOfKey - i)
         key[j], key[lenghtOfKey - i] = key[lenghtOfKey - i], key[j]
